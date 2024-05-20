81fc22315c6b5cbf57aae8945513859367308818
Richard Burman <richard.burman@harperjames.co.uk>
2024-05-20 14:37:18 +0100

    Ingredients passing from recipes to shopping list via Services

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css'
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
 @Input() recipe: Recipe;  
+
+constructor(private recipeService: RecipeService) {}
+
+ngOnInit() {
+  
+}
+
+onAddToShoppingList() {
+  this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+}
 
 }
