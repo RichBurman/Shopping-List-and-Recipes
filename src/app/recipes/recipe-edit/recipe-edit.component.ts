b43f68718f904b13fa5efa93541991a23dc664df
Richard Burman <richard.burman@harperjames.co.uk>
2024-05-28 16:20:19 +0100

    Edit routes and create recipe edit component

---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrl: './recipe-edit.component.css'
+})
+
+export class RecipeEditComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) {}
+  id: number;
+  editMode = false;
+
+  ngOnInit() {
+    this.route.params
+    .subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.editMode = params['id'] != null;
+        console.log(this.editMode)
+      }
+    )
+  }
+}
