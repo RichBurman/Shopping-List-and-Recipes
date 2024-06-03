d47055ff1d36300e172fd79b60796cc7f10979d4
Richard Burman <richard.burman@harperjames.co.uk>
2024-06-03 13:31:16 +0100

    Config data-storage service to allow Recipes to be stored in firebase API

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
+
+import { RecipeService } from "../recipes/recipe.service";
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private recipesService: RecipeService) {}
+
+    storeRecipes() {
+        const recipes = this.recipesService.getRecipes();
+        this.http.put('https://recipe-book-a5516-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes).subscribe(response => {
+            console.log(response);
+        });
+    }
 }
