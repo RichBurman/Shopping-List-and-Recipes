c3338972a05fb498cf86134cf864cd7280de340f
Richard Burman <richard.burman@harperjames.co.uk>
2024-06-04 09:00:33 +0100

    Sending the signup request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,20 +1,40 @@
-import { Component } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
-@Component( {
-    selector: 'app-auth', 
-    templateUrl: './auth.component.html'
+import { AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
 })
-
 export class AuthComponent {
-    isLoginMode = true;
+  isLoginMode = true;
 
-    onSwitchMode() {
-        this.isLoginMode = !this.isLoginMode;
-    }
+  constructor(private authService: AuthService) {}
 
-    onSubmit(form: NgForm) {
-        console.log(form.value);
-        form.reset();
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
     }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+        // ...
+    } else {
+        this.authService.signup(email, password).subscribe(
+            (resData) => {
+              console.log(resData);
+            },
+            (error) => {
+              console.log(error);
+            }
+          );
+    }
+    form.reset();
+  }
 }
