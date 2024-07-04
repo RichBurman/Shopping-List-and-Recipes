bc5d924b654c23a3970cbf4d7092c22ecd72a3f4
Richard Burman <richard.burman@harperjames.co.uk>
2024-07-04 10:25:14 +0100

    Add lazy loading for shopping list and auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 
 
 import { AuthComponent } from "./auth/auth.component";
+import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) }
+    { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) },
+    { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule) },
+    { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
 ];
 
 @NgModule({
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -16,7 +16,7 @@ import { SharedModule } from "../shared/shared.module";
     
         FormsModule,
         RouterModule.forChild([
-            { path: 'shopping-list', component: ShoppingListComponent},
+            { path: '', component: ShoppingListComponent},
         ]),
         SharedModule
     ]
